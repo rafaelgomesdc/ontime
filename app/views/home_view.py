fa09c826b1bdce2f4745bcb0b775cfb0140f9353
rafaelgomesdc <rafaelgomesdc0000@gmail.com>
2026-07-26 12:34:38 -0300

    feat: cronômetro de trabalho

--- app/views/home_view.py
+++ app/views/home_view.py
@@ -8,9 +8,11 @@ sys.path.append(os.path.abspath("../services"))
 
 from app.services.ponto import Ponto
 from utils.utils import relogio
+from utils.utils import cronometro
 
 class Home_view:
     ponto_controller = Ponto()
+    is_running = True #Cronômetro de tempo trabalhado
 
     def __init__(self, page: ft.Page):
         self.page = page
@@ -24,11 +26,22 @@ class Home_view:
 
         self.ui_relogio = ft.Text( #Relógio
             value="00:00:00",
+            size=20
+        )
+
+        #Cronômetro
+        self.ui_ttl_work_timer = ft.Text( #Título do cronômetro 
+            value="Tempo de trabalho",
+            size=20
+        )
+        self.ui_work_timer = ft.Text( #Cronômetro de tempo trabalhado
+            value="00:00:00",
             size=40
         )
 
+        #Marcação de ponto
         self.bt_marcar_ponto = ft.ElevatedButton( #Marcar ponto
-            "Marcar Ponto", 
+            "Marcar Ponto",
             on_click=self.Marcar_ponto
         )
 
@@ -38,6 +51,7 @@ class Home_view:
         )
 
         self.page.run_task(relogio, self) #Inicia o funcionamento do relógio
+        self.page.run_task(cronometro, self) #Inicia o funcionamento do cronômetro
 
         self.row_titulo = ft.Row(
             self.ui_titulo,
@@ -47,6 +61,8 @@ class Home_view:
             ft.Column(
                 [
                     self.ui_relogio,
+                    self.ui_ttl_work_timer,
+                    self.ui_work_timer,
                     self.bt_marcar_ponto,
                     self.ui_txt_ultima_marcacao
                 ],
@@ -66,4 +82,9 @@ class Home_view:
     async def Marcar_ponto(self):
         #Ação do botão "marcar ponto"
         self.ponto_controller.Registrar_Ponto(self.page, ft)
+        if self.is_running:
+            self.is_running = False
+        else:
+            self.is_running = True
+        print(self.is_running)
         self.page.update()
--- utils/utils.py
+++ utils/utils.py
@@ -9,4 +9,48 @@ async def relogio(home):
 
         await asyncio.sleep(1)
 
+async def cronometro(home):
+    #time = 0
+
+    s = 0
+    m = 0
+    h = 0
+
+    s_text = "00"
+    m_text = "00"
+    h_text = "00"
+
+    while True:
+        if home.is_running:
+            #Contagem
+            if s < 59:
+                s += 1
+            elif s == 59:
+                m += 1
+                s = 0
+
+            if m > 59:
+                h += 1
+                m = 0
+
+            #Formatação
+            if s < 10:
+                s_text = f"0{s}"
+            else:
+                s_text = f"{s}"
+
+            if m < 10:
+                m_text = f"0{m}"
+            else:
+                m_text = f"{m}"
+            if h < 10:
+                h_text = f"0{h}"
+            else:
+                h_text = f"{h}"
+
+            home.ui_work_timer.value = (f"{h_text}:{m_text}:{s_text}")
+            home.ui_work_timer.update()
+
+        await asyncio.sleep(1)
+
     
